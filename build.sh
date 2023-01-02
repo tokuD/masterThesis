3fda3534a1783b974deb2d68d84747d394799381
tokuD <megumu112851@gmail.com>
2023-01-02 20:17:31 +0900

    add chapter3

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ platex main.tex
 dvipdfmx main
 platex main.tex
 dvipdfmx main
-rm main.aux main.dvi main.lof main.lot main.toc main.log
+rm main.aux main.dvi main.lof main.lot main.toc main.log 'main.synctex(busy)'
 
 open main.pdf
